711d42173b971060a290eb4110da5c0c11fca02e
Soumil <soumil.vavikar@gmail.com>
2024-09-20 09:13:14 -0400

    fixed issues with VS Code

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script, console} from "lib/forge-std/src/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
